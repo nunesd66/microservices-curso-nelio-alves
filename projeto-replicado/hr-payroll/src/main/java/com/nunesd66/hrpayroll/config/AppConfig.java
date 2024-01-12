2fe19359a2785d6e4d3e44d3b6a5ee48440c377a
Daniel <nunes_daniel@outlook.com.br>
2024-01-12 17:13:35 -0300

    RestTemplate

---
+++ projeto-replicado/hr-payroll/src/main/java/com/nunesd66/hrpayroll/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.nunesd66.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+	
+    @Bean
+    RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+}
