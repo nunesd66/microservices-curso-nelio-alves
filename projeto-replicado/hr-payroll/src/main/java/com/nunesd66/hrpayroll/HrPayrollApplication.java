cd35123e545e0b17767c40ed5cacf90414d5c996
Daniel <nunes_daniel@outlook.com.br>
2024-01-13 08:00:40 -0300

    Feign client

--- projeto-replicado/hr-payroll/src/main/java/com/nunesd66/hrpayroll/HrPayrollApplication.java
+++ projeto-replicado/hr-payroll/src/main/java/com/nunesd66/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.nunesd66.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
